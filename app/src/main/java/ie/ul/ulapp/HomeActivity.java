dccc025f565b3a669cc8d6250cec1eddd7b0e58c
Nicole <nicole.berty@hotmail.com>
2021-03-03 16:11:30 +0000

    Add Recyclerview for home page

--- app/src/main/java/ie/ul/ulapp/HomeActivity.java
+++ app/src/main/java/ie/ul/ulapp/HomeActivity.java
@@ -3,18 +3,25 @@ package ie.ul.ulapp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
 
     private FirebaseAuth mAuth;
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         mAuth = FirebaseAuth.getInstance();
+
+        List<String> names = getNameList();
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, names);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
     public void onClick(View view) {
@@ -36,5 +50,40 @@ public class HomeActivity extends AppCompatActivity {
                         }
                     });
         }
+    }
+
+    private List<String> getNameList() {
+        List<String> names = new ArrayList<>();
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        names.add("kevin");
+        return names;
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, " You clicked " + adapter.getItem(position) + " on the row number " + position,
+                Toast.LENGTH_SHORT).show();
     }
 }
