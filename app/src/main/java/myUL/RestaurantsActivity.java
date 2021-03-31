578c0afa1aea897fdc57b97b6fe944846bc1cb3a
Nicole <nicole.berty@hotmail.com>
2021-03-31 21:40:37 +0100

    Rename activity_main.xml to activity_restaurants.xml

--- app/src/main/java/myUL/RestaurantsActivity.java
+++ app/src/main/java/myUL/RestaurantsActivity.java
@@ -11,7 +11,7 @@ public class RestaurantsActivity extends MenuActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_restaurants);
         //fragment = (Fragment)findViewById(R.id.fragment);
     }
 }
