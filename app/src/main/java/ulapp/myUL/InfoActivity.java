3b031ebd1739fb6cb5f7831462794cc0f960f3bb
Nicole <nicole.berty@hotmail.com>
2021-04-12 00:06:50 +0100

    Comment info activity class

--- app/src/main/java/ulapp/myUL/InfoActivity.java
+++ app/src/main/java/ulapp/myUL/InfoActivity.java
@@ -25,13 +25,16 @@ public class InfoActivity extends ActionBar {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
+        //Set the names of the ImageView in the activity
+
         ImageView ULImage = findViewById(R.id.InfoPageImage);
         ImageView contactInfoImage = findViewById(R.id.contactInfoImage);
-
         ImageView parkingImage = findViewById(R.id.ParkingInfoImage);
-
         ULImage.setImageResource(R.drawable.ul_logo);
         contactInfoImage.setImageResource(R.drawable.contact);
+        parkingImage.setImageResource(R.drawable.parking);
+
+        //Set the intent for when each ImageView is clicked
         contactInfoImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -41,7 +44,6 @@ public class InfoActivity extends ActionBar {
             }
         });
 
-        parkingImage.setImageResource(R.drawable.parking);
         parkingImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -50,12 +52,20 @@ public class InfoActivity extends ActionBar {
                 startActivity(intent);
             }
         });
-        checkDatabase();
+
+        //Call function to check if user is a student or not
+        checkIfStudent();
     }
 
+    /**
+     * Add the guides ImageView button to the page. This method is only called if the user is a student registered in the userTypes collection of the database
+     */
     public void addGuides() {
         ImageView guidesImage = findViewById(R.id.GuidesInfoImage);
+        //Set ImageView picture
         guidesImage.setImageResource(R.drawable.guides);
+
+        //User will be taken to the GuidesActivity when clicking the guides image
         guidesImage.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -65,31 +75,35 @@ public class InfoActivity extends ActionBar {
             }
         });
     }
-    public void checkDatabase() {
+
+    /**
+     * Method which queries the Firebase Firestore database if the current user is not anonymous to see if they are a student in the user types collection
+     */
+    public void checkIfStudent() {
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         String email = "";
-        //User is logged in
+        //User is logged in and registered on the database
         if (currentUser != null && !currentUser.isAnonymous()) {
             email = currentUser.getEmail();
             if (email != null || !email.equals("")) {
-                //Connect to Firebase FireStore
+                //Connect to Firebase Firestore
                 FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                //Get a reference to the userTypes collection in Firestore
                 DocumentReference docRef = db.collection("userTypes").document(email);
                 docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
                             DocumentSnapshot document = task.getResult();
+                            //The document exists which means the user's email is in the userTypes collection
                             if (document.exists()) {
-                                Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                                //The document data is returned as an object map. Convert to string, split it on the = and remove the }
                                 String[] values = document.getData().toString().split("=");
                                 String userType = values[1].replace("}", "");
-                                Log.d("TAG", "USER TYPE data: " + userType);
+                                //If the user is a student, add the guides button to the info page
                                 if (userType.equals("student")) {
-                                    Log.d("tag", "am student");
                                     addGuides();
-                                } else {
-                                    Log.d("tag", "am not");
                                 }
                             } else {
                                 Log.d("TAG", "No such document");
