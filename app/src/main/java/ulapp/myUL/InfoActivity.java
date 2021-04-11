c741fbba5be9e49daca8ff09cf86b0a4a6f1bc36
Nicole <nicole.berty@hotmail.com>
2021-04-11 23:54:35 +0100

    Add database check to Info Activity so that only registered users who are students can see the user guides button and access that page. Update text in guides fragment and strings.xml for guides.

--- app/src/main/java/ulapp/myUL/InfoActivity.java
+++ app/src/main/java/ulapp/myUL/InfoActivity.java
@@ -2,8 +2,19 @@ package ulapp.myUL;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import ulapp.myUL.guides.GuidesActivity;
 
@@ -16,7 +27,7 @@ public class InfoActivity extends ActionBar {
 
         ImageView ULImage = findViewById(R.id.InfoPageImage);
         ImageView contactInfoImage = findViewById(R.id.contactInfoImage);
-        ImageView guidesImage = findViewById(R.id.GuidesInfoImage);
+
         ImageView parkingImage = findViewById(R.id.ParkingInfoImage);
 
         ULImage.setImageResource(R.drawable.ul_logo);
@@ -29,15 +40,7 @@ public class InfoActivity extends ActionBar {
             startActivity(intent);
             }
         });
-        guidesImage.setImageResource(R.drawable.guides);
-        guidesImage.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InfoActivity.this, GuidesActivity.class);
-                startActivity(intent);
-            }
-        });
         parkingImage.setImageResource(R.drawable.parking);
         parkingImage.setOnClickListener(new View.OnClickListener() {
 
@@ -47,6 +50,56 @@ public class InfoActivity extends ActionBar {
                 startActivity(intent);
             }
         });
+        checkDatabase();
     }
 
+    public void addGuides() {
+        ImageView guidesImage = findViewById(R.id.GuidesInfoImage);
+        guidesImage.setImageResource(R.drawable.guides);
+        guidesImage.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InfoActivity.this, GuidesActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public void checkDatabase() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String email = "";
+        //User is logged in
+        if (currentUser != null && !currentUser.isAnonymous()) {
+            email = currentUser.getEmail();
+            if (email != null || !email.equals("")) {
+                //Connect to Firebase FireStore
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                DocumentReference docRef = db.collection("userTypes").document(email);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+                                String[] values = document.getData().toString().split("=");
+                                String userType = values[1].replace("}", "");
+                                Log.d("TAG", "USER TYPE data: " + userType);
+                                if (userType.equals("student")) {
+                                    Log.d("tag", "am student");
+                                    addGuides();
+                                } else {
+                                    Log.d("tag", "am not");
+                                }
+                            } else {
+                                Log.d("TAG", "No such document");
+                            }
+                        } else {
+                            Log.d("TAG", "get failed with ", task.getException());
+                        }
+                    }
+                });
+            }
+        }
+    }
 }
